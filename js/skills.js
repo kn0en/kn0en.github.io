8a708dd9a5ca8b69ba1e085461af36d88e9a5106
kn0en <pratamaoktavian66@gmail.com>
2020-11-06 05:03:21 +0700

    fix some bugs

--- js/skills.js
+++ js/skills.js
@@ -1,10 +1,14 @@
-var i = 0;
 function move() {
-  if (i == 0) {
     i = 1;
     var elem = document.getElementById("progress1");
+    var elem1 = document.getElementById("progress2");
+    var elem2 = document.getElementById("progress3");
+    var elem3 = document.getElementById("progress4");
     var width = 1;
-    var id = setInterval(frame, 20);
+    var id = setInterval(frame, 25);
+    var id1 = setInterval(frame1, 25);
+    var id2 = setInterval(frame2, 25);
+    var id3 = setInterval(frame3, 20);
     function frame() {
       if (width >= 90) {
         clearInterval(id);
@@ -14,7 +18,33 @@ function move() {
         elem.style.width = width + "%";
       }
     }
-  }
+    function frame1() {
+      if (width >= 80) {
+        clearInterval(id1);
+        i = 0;
+      } else {
+        width++;
+        elem1.style.width = width + "%";
+      }
+    }
+    function frame2() {
+      if (width >= 65) {
+        clearInterval(id2);
+        i = 0;
+      } else {
+        width++;
+        elem2.style.width = width + "%";
+      }
+    }
+    function frame3() {
+      if (width >= 60) {
+        clearInterval(id3);
+        i = 0;
+      } else {
+        width++;
+        elem3.style.width = width + "%";
+      }
+    }
 }
 
 window.addEventListener("load", function(){
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,4 +1,4 @@
-var i = 0;
+var k = 0;
 var j = 0;
 var txt = 'Hello, world!';
 var txt1 = "This site is a simple website that contains information about myself, my experiences, the skills I have learned and some of the projects I've worked on.";
@@ -6,9 +6,9 @@ var speed = 100;
 var speed1 = 50;
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("helloWorld").innerHTML += txt.charAt(i);
-    i++
+  if (k < txt.length) {
+    document.getElementById("helloWorld").innerHTML += txt.charAt(k);
+    k++
     setTimeout(typeWriter, speed);
   }
 }
